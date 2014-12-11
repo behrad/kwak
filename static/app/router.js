0debd8ae7acafe4a407580d2ce2add8016bd504b
Victor Felder <victorfelder@gmail.com>
2014-12-11 10:29:16 +0100

    channels list

--- static/app/router.js
+++ static/app/router.js
@@ -6,7 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('channel');
+  this.route('channels');
+  this.route('channel', { path: '/channel/:channel_id/:name'});
 });
 
 export default Router;
