dfb6a80db6e0e1aacbae47cd535a5e8af33ba04b
Victor Felder <victorfelder@gmail.com>
2014-12-12 22:15:35 +0100

    TODO: push real objects

--- static/app/controllers/channels/topic.js
+++ static/app/controllers/channels/topic.js
@@ -6,11 +6,17 @@ export default Ember.ObjectController.extend({
     createMessage() {
       var msg = this.get('message');
 
+      //TODO fix this mess
+      var aSampleTopic = this.store.getById('topic', 3);
+      var aSampleUser = this.store.getById('user', 1);
+
+
       var message = this.store.createRecord('message', {
         content: msg,
-        topic: this.store.find('topic', 1),
-        author: this.store.find('user', 1)
+        topic: aSampleTopic,
+        author: aSampleUser
       });
+
       message.save();
     }
   }
