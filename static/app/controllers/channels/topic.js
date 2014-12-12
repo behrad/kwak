3a0e73dd16c95070ce2f019f8b7ef9301b28d16d
Victor Felder <victorfelder@gmail.com>
2014-12-12 23:08:16 +0100

    moar todos

--- static/app/controllers/channels/topic.js
+++ static/app/controllers/channels/topic.js
@@ -4,20 +4,31 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   actions: {
     createMessage() {
-      var msg = this.get('message');
+      var content = this.get('message');
+      if (!content.trim()) { return; }
+
+      var channel_id = this.get('topic.channel.id');
+      if (!channel_id.trim()) { return; }
+
+      var topic_title = this.get('topic.title');
+      if (!topic_title.trim()) { return; }
 
       //TODO fix this mess
       var aSampleTopic = this.store.getById('topic', 3);
+      //TODO this is supposed to be the *current* user
       var aSampleUser = this.store.getById('user', 1);
 
 
       var message = this.store.createRecord('message', {
-        content: msg,
+        content: content,
         topic: aSampleTopic,
         author: aSampleUser
       });
 
+      this.set('message', '');
+
       message.save();
+
     }
   }
 });
