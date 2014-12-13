b2bd2befe4a3e49565dc07f1c9e89ec788526e1f
Victor Felder <victorfelder@gmail.com>
2014-12-13 12:34:35 +0100

    api/me leverage

--- static/app/controllers/channels/topic.js
+++ static/app/controllers/channels/topic.js
@@ -15,19 +15,22 @@ export default Ember.ObjectController.extend({
 
       //TODO fix this mess
       var aSampleTopic = this.store.getById('topic', 3);
-      //TODO this is supposed to be the *current* user
-      var aSampleUser = this.store.getById('user', 1);
 
+      $.getJSON( "/api/me", function( data ) {
+        var user_id = data['users'][0]['id'];
+        var user = this.store.getById('user', user_id);
 
-      var message = this.store.createRecord('message', {
-        content: content,
-        topic: aSampleTopic,
-        author: aSampleUser
+        var message = this.store.createRecord('message', {
+          content: content,
+          topic: aSampleTopic,
+          author: user
+        });
+
+        this.set('message', '');
+
+        message.save();
+
       });
-
-      this.set('message', '');
-
-      message.save();
 
     }
   }
