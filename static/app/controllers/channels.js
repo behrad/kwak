cbbcd5494bfa26d76d305e2d27883eafaf60b681
Victor Felder <victorfelder@gmail.com>
2014-12-16 14:04:19 +0100

    filtered arrangedcontent channels

--- static/app/controllers/channels.js
+++ static/app/controllers/channels.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   sortProperties: ['name'],
-  sortAscending: true
+  sortAscending: true,
+
+  subscribed: function() {
+    return this.get('arrangedContent').filterProperty('subscribed', true);
+  }.property('model.@each.subscribed')
 });
