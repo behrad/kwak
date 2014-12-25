7568d9f274a301219836b3646cb92c3bd76dc1ec
Victor Felder <victorfelder@gmail.com>
2014-12-25 20:37:05 +0100

    Removed buggy code

--- static/app/controllers/channels/channel/mark-as-read.js
+++ static/app/controllers/channels/channel/mark-as-read.js
@@ -5,8 +5,6 @@ export default Ember.ObjectController.extend({
     markAsRead: function (messageId) {
       if (messageId) {
         this.store.find('message', messageId).then(function (message) {
-          window.cursor = messageId;
-          message.get('topic.channel').set('cursor', messageId);
           message.set('seen', true);
           Ember.Logger.debug('TODO: save somehow. Read:', messageId);
         });
--- static/app/models/channel.js
+++ static/app/models/channel.js
@@ -6,19 +6,4 @@ export default DS.Model.extend({
   topics: DS.hasMany('topic'),
   team: DS.belongsTo('team'),
   subscribed: DS.attr('boolean'),
-  cursor: DS.attr('integer'),
-  unread: function () {
-    var topics = this.get('topics');
-    var messages;
-    var ids = [];
-    for (var i = 0; i < topics.get('length'); i++) {
-      messages = topics.objectAt(i).get('messages');
-      for (var j = 0; j < messages.get('length'); j++) {
-        ids.push(messages.objectAt(j).get('id'));
-      }
-    }
-    var unreadIds = ids.filter(function(i) { return i > window.cursor; });
-    console.log(unreadIds);
-    return unreadIds.length;
-  }.property('cursor')
 });
