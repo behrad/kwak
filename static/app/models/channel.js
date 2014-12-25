7a582a21d02bbc6ecd1111b7af502ffb590f1efc
Victor Felder <victorfelder@gmail.com>
2014-12-25 20:12:23 +0100

    unread

--- static/app/models/channel.js
+++ static/app/models/channel.js
@@ -5,5 +5,18 @@ export default DS.Model.extend({
   color: DS.attr('string'),
   topics: DS.hasMany('topic'),
   team: DS.belongsTo('team'),
-  subscribed: DS.attr('boolean')
+  subscribed: DS.attr('boolean'),
+  unread: function () {
+    var cursor = 25;
+    var topics = this.get('topics');
+    var messages;
+    var ids = [];
+    for (var i = 0; i < topics.get('length'); i++) {
+      messages = topics.objectAt(i).get('messages');
+      for (var j = 0; j < messages.get('length'); j++) {
+        ids.push(messages.objectAt(j).get('id'));
+      }
+    }
+    return ids.filter(function(i) { return i > cursor; }).length;
+  }.property('profile.cursor')
 });
