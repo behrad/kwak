b130433d5d69e0acf04e7b37e3d90107d55a3268
Victor Felder <victorfelder@gmail.com>
2014-12-27 14:11:11 +0100

    scroll to window.cursor

--- static/app/mixins/setup-view.js
+++ static/app/mixins/setup-view.js
@@ -7,17 +7,19 @@ export default Ember.Mixin.create({
     this._super();
     Ember.run.scheduleOnce('afterRender', this, function () {
       window.prettyPrint();
+      window.scroll(0, window.cursor);
 
-      // $('.message').each(function() {
-      //   var $this = $(this);
-      //   var $next = $this.next();
-      //   if ($this.attr('data-topic-id') === $next.attr('data-topic-id')) {
-      //     $next.find('.message-header').hide();
-      //     $this.children('div').css('border-bottom', 0);
-      //     $this.css('margin', '0px');
-      //     $this.css('margin-bottom', '2px');
-      //   }
-      // });
+      $('.message').each(function() {
+        var $this = $(this);
+        var $next = $this.next();
+        if ($this.attr('data-topic-id') === $next.attr('data-topic-id')) {
+          $next.find('.message-header').hide();
+          $next.find('h5').eq(0).css('margin-top', 0);
+          $this.children('div').css('border-bottom', 0);
+          $this.css('margin', '0px');
+          $this.css('margin-bottom', '2px');
+        }
+      });
     });
   },
 });
