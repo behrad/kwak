0734a1f16eb385048ddaac558675ff0cb3f4fdec
Victor Felder <victorfelder@gmail.com>
2015-01-09 22:19:27 +0100

    Test mixpanel

--- static/app/router.js
+++ static/app/router.js
@@ -5,6 +5,17 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
+Ember.Router.reopen({
+  didTransition: function(paths){
+    this._super(paths);
+    Ember.run.next(function(){
+      var path = window.location.href;
+      mixpanel.track("pageview", {"url": path });
+    });
+  }
+});
+
+
 Router.map(function () {
 
   this.route('login');
