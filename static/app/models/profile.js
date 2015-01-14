fd42361e7b74f977c197d208105f003f178a8803
Victor Felder <victorfelder@gmail.com>
2015-01-14 16:06:24 +0100

    Add signup link to admin

--- static/app/models/profile.js
+++ static/app/models/profile.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   email: DS.attr('string'),
   cursor: DS.attr('number'),
-  team: DS.hasMany('team'),
+  teams: DS.hasMany('team'),
   is_admin: DS.attr('boolean'),
   is_active: DS.attr('boolean', {defaultValue: true}),
   unreadPm: function () {
