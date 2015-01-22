90d4d14db87c8d98dedb4b16c7ac17359896b4bc
Victor Felder <victorfelder@gmail.com>
2015-01-22 09:28:26 +0100

    make it work w/ember server

--- client/config/environment.js
+++ client/config/environment.js
@@ -25,6 +25,12 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV['contentSecurityPolicy'] = {
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
+      'connect-src': "'self' ws://localhost:8444 http://localhost:8444 http://localhost:8001",
+      'style-src': "'self' 'unsafe-inline'"
+    };
   }
 
   if (environment === 'test') {
