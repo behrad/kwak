bbbf31fc66d3948cb209e47a88f9574d6d4d63fd
Victor Felder <victorfelder@gmail.com>
2015-01-25 19:06:59 +0100

    Restrict SE to authenticated users

--- search/views.py
+++ search/views.py
@@ -3,20 +3,22 @@ from django.http import HttpResponse
 from forms import MessageSearchForm
 
 def search(request):
-    query = request.GET.get('q', '')
+    output = []
+    if request.user.is_authenticated():
+        query = request.GET.get('q', '')
 
-    profile = request.user.profile
-    teams = profile.teams.all().values_list('name', flat=True)
+        profile = request.user.profile
+        teams = profile.teams.all().values_list('name', flat=True)
 
-    # we retrieve the query to display it in the template
-    form = MessageSearchForm(request.GET)
+        # we retrieve the query to display it in the template
+        form = MessageSearchForm(request.GET)
 
-    # we call the search method from the MessageSearchForm. Haystack do the work!
-    results = form.search()
-    output = [{
-        'content': o.text.split("\n", 2),
-        'channel': o.channel,
-        'url': o.thread_url
-    } for o in results if o.team in teams]
+        # we call the search method from the MessageSearchForm. Haystack do the work!
+        results = form.search()
+        output = [{
+            'content': o.text.split("\n", 2),
+            'channel': o.channel,
+            'url': o.thread_url
+        } for o in results if o.team in teams]
 
     return HttpResponse(json.dumps({'results': output}), content_type='application/json')
