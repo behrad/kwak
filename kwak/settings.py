2b079495369a3a4ec347853817eb3bfb800a0f23
Victor Felder <victorfelder@gmail.com>
2015-01-26 16:40:06 +0100

    Install django-axes

--- kwak/settings.py
+++ kwak/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'axes',
     'rest_framework',
     'rest_framework.authtoken',
     'haystack',
@@ -50,6 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'axes.middleware.FailedLoginMiddleware',
 )
 
 ROOT_URLCONF = 'kwak.urls'
