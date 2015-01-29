7156c0bb6a3ef1f9dd6780028603906d4d39a08c
Victor Felder <victorfelder@gmail.com>
2015-01-29 20:48:43 +0100

    Get rid of autoreload

--- client/app/controllers/application.js
+++ client/app/controllers/application.js
@@ -3,22 +3,7 @@ import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
   authenticator: 'simple-auth-authenticator:token',
-  init: function () {
-    var now = new Date();
-
-    if (! window.localStorage.hasOwnProperty('timestamp')) {
-      window.localStorage['timestamp'] = now.getTime();
-    }
-
-    if (now.getTime() - window.localStorage['timestamp'] > 30000) {
-      window.localStorage['timestamp'] = now.getTime();
-      window.location.reload();
-    }
-  },
   actions: {
-    reload: function () {
-      window.location.reload();
-    },
     tour: function () {
       var is_admin = this.get('model.is_admin');
       var tour = new Shepherd.Tour({
