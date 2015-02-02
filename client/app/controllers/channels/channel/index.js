26456b0813508330f083563ed324c181e719617f
Victor Felder <victorfelder@gmail.com>
2015-02-02 22:17:41 +0100

    deduplicate flash message

--- client/app/controllers/channels/channel/index.js
+++ client/app/controllers/channels/channel/index.js
@@ -33,9 +33,6 @@ export default Ember.ObjectController.extend({
       var topicTitle = this.get('topicTitle');
       if (!topicTitle.trim()) { this.get('flashes').warning('Please enter a topic'); return; }
 
-      mixpanel.track("new message");
-      this.get('flashes').success('Message posted!');
-
       this.get('controllers.channels/channel/message-create').send(
         'createMessage',
         channel,
