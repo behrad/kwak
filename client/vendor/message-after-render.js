6477e83cd01ef2db82cbe384b5207f9f2a43c6f0
Victor Felder <victorfelder@gmail.com>
2015-02-02 20:35:05 +0100

    Successful makeup

--- client/vendor/message-after-render.js
+++ client/vendor/message-after-render.js
@@ -6,15 +6,39 @@ function messageAfterRender () {
 
   window.prettyPrint();
 
-  $('.message').each(function () {
+  var counter = 0;
+  var groups = [];
+
+  $('.message-list > .message').each(function () {
     var $this = $(this);
+    var $prev = $this.prev();
     var $next = $this.next();
+
+    if ($this.attr('data-topic-id') !== $prev.attr('data-topic-id')) {
+      console.log('new group '+ $this.attr('data-message-id'));
+      counter++;
+      groups[counter] = [];
+      groups[counter].push($this);
+    }
+
     if ($this.attr('data-topic-id') === $next.attr('data-topic-id')) {
+      console.log('continuation ' + $this.attr('data-message-id'));
       $next.find('.message-header').hide();
       $next.find('h5').eq(0).css('margin-top', 0);
       $this.children('div').css('border-bottom', 0);
       $this.css('margin', '0');
       $this.css('margin-bottom', '2px');
+      groups[counter].push($next);
     }
+  });
+
+  $('.message-list > .message').remove();
+
+  groups.forEach(function (group) {
+    var $group = $('<div class="group"></div>');
+    group.forEach(function (message) {
+      message.appendTo($group);
+    });
+    $group.appendTo('.message-list');
   });
 }
